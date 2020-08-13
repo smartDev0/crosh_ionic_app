9b37dcbce18ab1a8758cdb980359a1220feb3eca
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-08-13 16:15:29 +0100

    add  logout function in profile page

--- src/app/pages/account/profile/profile.component.ts
+++ src/app/pages/account/profile/profile.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "./../../../shared/service/authentication.service";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss'],
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.scss"],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  constructor(public authService: AuthenticationService) {}
 
   ngOnInit() {}
-
+  logout() {
+    this.authService.SignOut();
+  }
 }
--- src/app/shared/service/authentication.service.ts
+++ src/app/shared/service/authentication.service.ts
@@ -149,7 +149,7 @@ export class AuthenticationService {
   SignOut() {
     return this.ngFireAuth.signOut().then(() => {
       localStorage.removeItem("user");
-      this.router.navigate(["login"]);
+      this.router.navigate(["welcome"]);
     });
   }
 }
